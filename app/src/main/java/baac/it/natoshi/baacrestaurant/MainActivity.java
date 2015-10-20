b7d7154d29d1abcb47e37649ee9c290f16dcf82b
natBAAC <unlimitz2010@gmail.com>
2015-10-20 09:45:18 +0700

    Delete All

--- app/src/main/java/baac/it/natoshi/baacrestaurant/MainActivity.java
+++ app/src/main/java/baac/it/natoshi/baacrestaurant/MainActivity.java
@@ -1,5 +1,6 @@
 package baac.it.natoshi.baacrestaurant;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,7 +11,6 @@ public class MainActivity extends AppCompatActivity {
     private FoodTABLE objFoodTABLE;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,10 +19,19 @@ public class MainActivity extends AppCompatActivity {
         createAndConnected();
 
         //Tester Add new value
-        testerAdd();
-        testerAddFood();
-
+        // testerAdd();
+        //  testerAddFood();
+        //Delete All SQLite
+        deleteAllSQLite();
     } // main Methodgit
+
+    private void deleteAllSQLite() {
+        SQLiteDatabase objSqLiteDatabase = openOrCreateDatabase("BAAC.db", MODE_PRIVATE, null);
+        objSqLiteDatabase.delete("userTABLE", null, null);
+        objSqLiteDatabase.delete("foodTABLE", null, null);
+    }
+
+
 
     private void testerAddFood() {
         objFoodTABLE.addNewFood("อาหาร ", "testSource", "1000");
@@ -30,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void testerAdd() {
 
-        objUserTABLE.addNewUser("testUSer","testPassword","พัฒนเกียรติ");
+        objUserTABLE.addNewUser("testUSer", "testPassword", "พัฒนเกียรติ");
     }
 
     private void createAndConnected() {
